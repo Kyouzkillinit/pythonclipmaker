47b4f026526ea251e01449a4e8ff832f70be9a02
Kyouzkillinit <46802533+Kyouzkillinit@users.noreply.github.com>
2022-04-02 16:30:37 -0600

    Create cut-script.py

---
+++ cut-script.py
@@ -0,0 +1,3 @@
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+# ffmpeg_extract_subclip("full.mp4", start_seconds, end_seconds, targetname="cut.mp4")
+ffmpeg_extract_subclip("Kay Flock - Is Ya Ready (shot by KLO Vizionz)-gGJS8W9emac-1080p-1646312374957.mp4", 14, 27, targetname="cut.mp4")
