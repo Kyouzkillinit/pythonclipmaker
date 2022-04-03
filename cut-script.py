d25e5caf519cdb2da477db574be6ab647cb7c6d5
Kyouzkillinit <46802533+Kyouzkillinit@users.noreply.github.com>
2022-04-03 17:24:00 -0600

    Update cut-script.py

--- cut-script.py
+++ cut-script.py
@@ -1,4 +1,5 @@
 from tkinter import filedialog as fd
+from moviepy.editor import VideoFileClip
 
 filename = fd.askopenfilename()
 
@@ -8,3 +9,7 @@ endtime = input("enter end-time (in seconds): ")
 
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 ffmpeg_extract_subclip((filename), 14, 27, targetname="cut.mp4")
+
+videoclip = VideoFileClip("cut.mp4")
+new_clip = videoclip.without_audio()
+new_clip.write_videofile("noaud_cut.mp4")
