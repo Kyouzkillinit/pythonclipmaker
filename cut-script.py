98f186f08922898f9e3207a796314416f26542fe
Kyouzkillinit <46802533+Kyouzkillinit@users.noreply.github.com>
2022-04-03 17:41:23 -0600

    Update cut-script.py

--- cut-script.py
+++ cut-script.py
@@ -10,6 +10,8 @@ endtime = input("enter end-time (in seconds): ")
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 ffmpeg_extract_subclip((filename), 14, 27, targetname="cut.mp4")
 
+# "14" is supposed to be (starttime) and 27 is supposed to be (endtime) but i cant seem to get it to work without errors
+
 videoclip = VideoFileClip("cut.mp4")
 new_clip = videoclip.without_audio()
 new_clip.write_videofile("noaud_cut.mp4")
