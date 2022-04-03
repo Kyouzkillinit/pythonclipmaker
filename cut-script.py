2e4580d0426cfc1e84069cfc5e4fd6201b44387c
Kyouzkillinit <46802533+Kyouzkillinit@users.noreply.github.com>
2022-04-03 17:16:07 -0600

    Update cut-script.py

--- cut-script.py
+++ cut-script.py
@@ -1,3 +1,7 @@
+from tkinter import filedialog as fd
+
+filename = fd.askopenfilename()
+
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 # ffmpeg_extract_subclip("your_video.mp4", start_seconds, end_seconds, targetname="cut.mp4")
-ffmpeg_extract_subclip("Kay Flock - Is Ya Ready (shot by KLO Vizionz)-gGJS8W9emac-1080p-1646312374957.mp4", 14, 27, targetname="cut.mp4")
+ffmpeg_extract_subclip((filename), 14, 27, targetname="cut.mp4")
