0c19d399cc5a298e81865ecde3f8af12bcce0a7c
Kyouzkillinit <46802533+Kyouzkillinit@users.noreply.github.com>
2022-04-03 17:48:57 -0600

    Update cut-script.py

--- cut-script.py
+++ cut-script.py
@@ -10,7 +10,3 @@ from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 ffmpeg_extract_subclip((filename), 14, 27, targetname="cut.mp4")
 
 # 14 is supposed to be (starttime) and 27 is supposed to be (endtime) but i cant seem to get it to work without errors, can anyone else fix this?
-
-python remaudioscript.py
-
-#ive made this 2 seperate scripts so it can run faster
