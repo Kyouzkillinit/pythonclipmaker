03f1648dfec29a85c3ad50c91b06c74ef9a97b5b
Kyouzkillinit <46802533+Kyouzkillinit@users.noreply.github.com>
2022-04-03 17:20:49 -0600

    Update cut-script.py

--- cut-script.py
+++ cut-script.py
@@ -2,6 +2,9 @@ from tkinter import filedialog as fd
 
 filename = fd.askopenfilename()
 
+starttime = input("enter start-time (in seconds): ")
+
+endtime = input("enter end-time (in seconds): ")
+
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
-# ffmpeg_extract_subclip("your_video.mp4", start_seconds, end_seconds, targetname="cut.mp4")
 ffmpeg_extract_subclip((filename), 14, 27, targetname="cut.mp4")
