9724ff571a2d5068a4adabc98bc0cbaca41c9950
Kyouzkillinit <46802533+Kyouzkillinit@users.noreply.github.com>
2022-04-03 18:04:26 -0600

    Update cut-script.py

--- cut-script.py
+++ cut-script.py
@@ -1,4 +1,5 @@
 from tkinter import filedialog as fd
+from moviepy.editor import VideoFileClip
 
 filename = fd.askopenfilename()
 
@@ -10,3 +11,8 @@ from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 ffmpeg_extract_subclip((filename), 14, 27, targetname="cut.mp4")
 
 # 14 is supposed to be (starttime) and 27 is supposed to be (endtime) but i cant seem to get it to work without errors, can anyone else fix this?
+
+videoclip = VideoFileClip("cut.mp4")
+new_clip = videoclip.without_audio()
+new_clip.write_videofile("noaud_cut.mp4")
+
