f928022e0c2c53336b533294300d62acfd21b420
ThaMastaCoda3000 <103164964+ThaMastaCoda3000@users.noreply.github.com>
2022-04-07 00:23:42 -0400

    Update cut-script.py

--- cut-script.py
+++ cut-script.py
@@ -1,6 +1,9 @@
+from moviepy.editor import *
+import time
 from tkinter import filedialog as fd
-from moviepy.editor import VideoFileClip
+from moviepy.editor import AudioFileClip
 
+print("select video file")
 filename = fd.askopenfilename()
 
 while True:
@@ -19,6 +22,12 @@ while True:
       print("Please input integer only...")  
       continue
 
+print("selct audio file")
+music = fd.askopenfilename()
+audio = AudioFileClip(music)
+
+outputFile = input("Name the output file")
+
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 ffmpeg_extract_subclip((filename), (starttime), (endtime), targetname="cut.mp4")
 
@@ -26,3 +35,23 @@ videoclip = VideoFileClip("cut.mp4")
 new_clip = videoclip.without_audio()
 new_clip.write_videofile("noaud_cut.mp4")
 
+auddur = int(audio.duration)
+print(type(auddur))
+#time.sleep(5000)
+
+clip = VideoFileClip("noaud_cut.mp4")
+numTimes = auddur/clip.duration
+
+print(str(auddur))
+
+#Resizing
+clip_resized = clip.resize((400, 200))
+
+#Code that doesn't work for looping
+loopedClip = clip_resized.loop(n=numTimes)
+
+# Set the audio of the clip
+loopedClip = loopedClip.set_audio(audio)
+
+# Export the clip
+loopedClip.write_videofile(outputFile + ".mp4", fps=10)
